e073da1eb719f441a7dbb6d10da735759d42a98b
Zaki Mubarok <kakzaki@gmail.com>
2021-09-29 15:42:54 +0700

    init

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'src/app.dart';
+import 'src/controllers/settings_controller.dart';
+import 'src/services/settings_service.dart';
+
+void main() async {
+  // Set up the Controller, which will glue user settings to multiple
+  // Flutter Widgets.
+  final settingsController = SettingsController(SettingsService());
+
+  // Load initial data while the splash screen is displayed.
+  // This prevents a sudden change when the app is first displayed.
+  await settingsController.loadSettings();
+
+  // Run the app and pass in the Controller. The app listens to the
+  // Controller for changes, then passes it further down to the View.
+  runApp(MyApp(settingsController: settingsController));
+}
