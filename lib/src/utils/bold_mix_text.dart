da29b0c6cebae19b66d3a007536f77bc2608f402
Zaki Mubarok <kakzaki@gmail.com>
2021-10-01 10:50:13 +0700

    add utils

---
+++ lib/src/utils/bold_mix_text.dart
@@ -0,0 +1,26 @@
+
+String firstFewWords(String bigSentence) {
+  int startIndex = 0, indexOfSpace=0;
+  for (int i = 0; i < 1; i++) {
+    indexOfSpace = bigSentence.indexOf(' ', startIndex);
+    if (indexOfSpace == -1) {
+      //-1 is when character is not found
+      return bigSentence;
+    }
+    startIndex = indexOfSpace + 1;
+  }
+  return bigSentence.substring(0, indexOfSpace);
+}
+
+String restOfWords(String bigSentence) {
+  int startIndex = 0, indexOfSpace=0;
+  for (int i = 0; i < 1; i++) {
+    indexOfSpace = bigSentence.indexOf(' ', startIndex);
+    if (indexOfSpace == -1) {
+      //-1 is when character is not found
+      return bigSentence;
+    }
+    startIndex = indexOfSpace + 1;
+  }
+  return bigSentence.substring(indexOfSpace);
+}
